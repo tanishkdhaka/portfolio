2b7a6a878c5717da9d80c3a90b09228f5f55dfb3
Tanishk Dhaka <er.tanishkdhaka@gmail.com>
2026-07-28 13:01:21 +0530

    Add Google Tag Manager scripts to layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -96,6 +96,16 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        {/* Google Tag Manager */}
+        <Script id="gtm-script" strategy="afterInteractive">
+          {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-KLR2SHW4');`}
+        </Script>
+        {/* End Google Tag Manager */}
+
         {/* ✅ Combined Person + WebSite Schema for richer Google results */}
         <Script
           id="schema-markup"
@@ -187,9 +197,18 @@ export default function RootLayout({
         />
       </head>
       <body className={`${bebasNeue.className} antialiased`}>
-      
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-KLR2SHW4"
+            height={0}
+            width={0}
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
+
         {children}
-        
       </body>
     </html>
   );
